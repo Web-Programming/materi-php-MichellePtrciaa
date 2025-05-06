15f0283855e2c0415628d2a98651c606594777ee
MichellePtrciaa <michellepatriciawijaya_2327240047@mhs.mdp.ac.id>
2025-05-06 17:33:43 +0700

    Menambahkan coding untuk layout
    Masih mengalami kesalahan

--- LATIHAN/resources/views/layout/footer.blade.php
+++ LATIHAN/resources/views/layout/footer.blade.php
@@ -1,3 +1,20 @@
 <div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+    <li class="user-footer">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <a href="#" class="btn btn-default btn-flat float-end">Sign out</a>
+    </li>
+    <footer class="app-footer">
+        <!--begin::To the end-->
+        <div class="float-end d-none d-sm-inline">Anything you want</div>
+        <!--end::To the end-->
+        <!--begin::Copyright-->
+        <strong>
+          Copyright &copy; 2014-2024&nbsp;
+          <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+        </strong>
+        All rights reserved.
+        <!--end::Copyright-->
+      </footer>
+      <!--end::Footer-->
+    </div>
 </div>
