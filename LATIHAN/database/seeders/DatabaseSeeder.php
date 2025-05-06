d75c3d7f9bbc3845a6096ef6c38ffb9e71a23fb6
MichellePatricia <michellepatriciawijaya_2327240047@mhs.mdp.ac.id>
2025-05-06 20:32:04 +0700

    Migration dan QueryBuilder

--- LATIHAN/database/seeders/DatabaseSeeder.php
+++ LATIHAN/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents; berfungsi untuk menanam/menginisialisasi 
+use DB;
+use Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+       //menggunakan query builder yg berfungsi untuk menjalankan query class database dari laravel
+       //panggil classnya
+    //    DB::table("users")->insert([
+    //         'name' => 'misel',
+    //         'email' => 'misel@gmail.com',
+    //         'password' => Hash::make("password")
+    //    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+       DB::table("users")
+        ->where("id", 1)
+        ->update([
+            'password' => Hash::make("123456")
         ]);
+
+        //DB::table("users")->where("id",">",1)->delete();
     }
 }
--- LATIHAN/resources/views/layout/header.blade.php
+++ LATIHAN/resources/views/layout/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="app-header navbar navbar-expand bg-body">
+   <nav class="app-header navbar navbar-expand bg-body">
         <!--begin::Container-->
         <div class="container-fluid">
           <!--begin::Start Navbar Links-->
@@ -194,4 +194,5 @@
           <!--end::End Navbar Links-->
         </div>
         <!--end::Container-->
-</nav>
+      </nav>
+      <!--end::Header-->
