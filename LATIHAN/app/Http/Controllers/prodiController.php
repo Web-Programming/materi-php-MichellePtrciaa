348827126bba45c4b1b8d41895ac8b8623e6a171
MichellePtrciaa <michellepatriciawijaya_2327240047@mhs.mdp.ac.id>
2025-05-13 20:38:10 +0700

    Menambahkan fungsi untuk prodi

--- LATIHAN/app/Http/Controllers/prodiController.php
+++ LATIHAN/app/Http/Controllers/prodiController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prodi;
+
 class ProdiController extends Controller{
-    function index(){
+    public function index(){
+        $listprodi = Prodi::get();
+        return view( "prodi.index");
+    }
+
+     public function create(){
+        
         return view( "prodi.index");
     }
 }
